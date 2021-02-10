920630ea3335d13f242e7d4cc055225893b6108a
Paul Tuck <pdrt888@gmail.com>
2021-02-11 02:24:43 +0800

    21-02-11T02:24

--- lib.php
+++ lib.php
@@ -21,85 +21,86 @@
  * @copyright  2021 Paul Tuck @pau1tuck
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-                                                        
+
 defined('MOODLE_INTERNAL') || die();
- 
-function theme_hskit_get_main_scss_content($theme) {                                                                                
-    global $CFG;                                                                                                                    
- 
-    $scss = '';                                                                                                                     
-    $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;                                                 
-    $fs = get_file_storage();                                                                                                       
- 
-    $context = context_system::instance();                                                                                          
-    if ($filename == 'default.scss') {                                                                                              
+
+function theme_hskit_get_main_scss_content($theme)
+{
+    global $CFG;
+
+    $scss = '';
+    $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;
+    $fs = get_file_storage();
+
+    $context = context_system::instance();
+    if ($filename == 'default.scss') {
         // We still load the default preset files directly from the boost theme. No sense in duplicating them.                      
-        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');                                        
-    } else if ($filename == 'plain.scss') {                                                                                         
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+    } else if ($filename == 'plain.scss') {
         // We still load the default preset files directly from the boost theme. No sense in duplicating them.                      
-        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');                                          
- 
-    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_hskit', 'preset', 0, '/', $filename))) {              
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');
+    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_hskit', 'preset', 0, '/', $filename))) {
         // This preset file was fetched from the file area for theme_hskit and not theme_boost (see the line above).                
-        $scss .= $presetfile->get_content();                                                                                        
-    } else {                                                                                                                        
+        $scss .= $presetfile->get_content();
+    } else {
         // Safety fallback - maybe new installs etc.                                                                                
-        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');                                        
-    }                                                                                                                                       
- 
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+    }
+
     // Pre CSS - this is loaded AFTER any pre-scss from the setting but before the main scss.                                        
-    $pre = file_get_contents($CFG->dirroot . '/theme/hskit/scss/pre.scss');                                                         
+    $pre = file_get_contents($CFG->dirroot . '/theme/hskit/scss/pre.scss');
     // Post CSS - this is loaded AFTER the main scss but before the extra scss from the setting.                                    
-    $post = file_get_contents($CFG->dirroot . '/theme/hskit/scss/post.scss');                                                       
- 
+    $post = file_get_contents($CFG->dirroot . '/theme/hskit/scss/post.scss');
+
     // Combine them together.                                                                                                       
-    return $pre . "\n" . $scss . "\n" . $post;                                                                                                                 
+    return $pre . "\n" . $scss . "\n" . $post;
 }
 
-function theme_hskit_update_settings_images($settingname) {                                                                         
-    global $CFG;                                                                                                                    
- 
+function theme_hskit_update_settings_images($settingname)
+{
+    global $CFG;
+
     // The setting name that was updated comes as a string like 's_theme_hskit_loginbackgroundimage'.                               
     // We split it on '_' characters.                                                                                               
-    $parts = explode('_', $settingname);                                                                                            
+    $parts = explode('_', $settingname);
     // And get the last one to get the setting name..                                                                               
-    $settingname = end($parts);                                                                                                     
- 
+    $settingname = end($parts);
+
     // Admin settings are stored in system context.                                                                                 
-    $syscontext = context_system::instance();                                                                                       
+    $syscontext = context_system::instance();
     // This is the component name the setting is stored in.                                                                         
-    $component = 'theme_hskit';                                                                                                     
- 
+    $component = 'theme_hskit';
+
     // This is the value of the admin setting which is the filename of the uploaded file.                                           
-    $filename = get_config($component, $settingname);                                                                               
+    $filename = get_config($component, $settingname);
     // We extract the file extension because we want to preserve it.                                                                
-    $extension = substr($filename, strrpos($filename, '.') + 1);                                                                    
- 
+    $extension = substr($filename, strrpos($filename, '.') + 1);
+
     // This is the path in the moodle internal file system.                                                                         
-    $fullpath = "/{$syscontext->id}/{$component}/{$settingname}/0{$filename}";                                                      
+    $fullpath = "/{$syscontext->id}/{$component}/{$settingname}/0{$filename}";
     // Get an instance of the moodle file storage.                                                                                  
-    $fs = get_file_storage();                                                                                                       
+    $fs = get_file_storage();
     // This is an efficient way to get a file if we know the exact path.                                                            
-    if ($file = $fs->get_file_by_hash(sha1($fullpath))) {                                                                           
+    if ($file = $fs->get_file_by_hash(sha1($fullpath))) {
         // We got the stored file - copy it to dataroot.                                                                            
         // This location matches the searched for location in theme_config::resolve_image_location.                                 
-        $pathname = $CFG->dataroot . '/pix_plugins/theme/hskit/' . $settingname . '.' . $extension;                                 
- 
+        $pathname = $CFG->dataroot . '/pix_plugins/theme/hskit/' . $settingname . '.' . $extension;
+
         // This pattern matches any previous files with maybe different file extensions.                                            
-        $pathpattern = $CFG->dataroot . '/pix_plugins/theme/hskit/' . $settingname . '.*';                                          
- 
+        $pathpattern = $CFG->dataroot . '/pix_plugins/theme/hskit/' . $settingname . '.*';
+
         // Make sure this dir exists.                                                                                               
-        @mkdir($CFG->dataroot . '/pix_plugins/theme/hskit/', $CFG->directorypermissions, true);                                      
- 
+        @mkdir($CFG->dataroot . '/pix_plugins/theme/hskit/', $CFG->directorypermissions, true);
+
         // Delete any existing files for this setting.                                                                              
-        foreach (glob($pathpattern) as $filename) {                                                                                 
-            @unlink($filename);                                                                                                     
-        }                                                                                                                           
- 
+        foreach (glob($pathpattern) as $filename) {
+            @unlink($filename);
+        }
+
         // Copy the current file to this location.                                                                                  
-        $file->copy_content_to($pathname);                                                                                          
-    }                                                                                                                               
- 
+        $file->copy_content_to($pathname);
+    }
+
     // Reset theme caches.                                                                                                          
-    theme_reset_all_caches();                                                                                                       
+    theme_reset_all_caches();
 }
